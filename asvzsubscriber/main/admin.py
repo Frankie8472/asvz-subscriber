a57b3df5d6cf3b88f7b043ff5eb6819582a903b6
Frankie <knobelf@student.ethz.ch>
2020-09-22 22:59:48 +0200

    encrypting and populating models

--- asvzsubscriber/main/admin.py
+++ asvzsubscriber/main/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
+from .models import ASVZEvent
 
 # Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
-        ("Content", {"fields": ["tutorial_content"]})
-    ]
+admin.site.register(ASVZEvent)
 
--- asvzsubscriber/main/models.py
+++ asvzsubscriber/main/models.py
@@ -1,4 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class ASVZEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    sport_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    event_start_date = models.DateTimeField()
+    register_start_date = models.DateTimeField()
+    url = models.URLField()
+
+    class Meta:
+        unique_together = ("user", "url")
+
+    def __str__(self):
+        return f"{self.user.__str__()} - {self.url[-6:]}"
