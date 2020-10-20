b4baabc120cbf24bc65cd7335868ad35fa51414e
Frankie <knobelf@student.ethz.ch>
2020-10-20 02:26:18 +0200

    implemented enrollment oveerview reordered crawler code TODO: save bearertoken with timer on it

--- asvzsubscriber/main/management/commands/timecheck.py
+++ asvzsubscriber/main/management/commands/timecheck.py
@@ -1,12 +1,9 @@
-import os
-from datetime import datetime, timezone
-from pathlib import Path
 import pytz
-from cryptography.fernet import Fernet
-from django.core.management import BaseCommand
-from main.event_subscriber import event_subscriber
-from main.models import ASVZEvent
+from datetime import datetime, timezone
 from pathos.multiprocessing import ProcessPool
+from django.core.management import BaseCommand
+from main.asvz_crawler import subscribe_to_event
+from main.models import ASVZEvent
 
 
 class Command(BaseCommand):
@@ -22,8 +19,6 @@ def check_time():
     event_list = ASVZEvent.objects.order_by('register_start_date')
 
     pool_event = []
-    pool_username = []
-    pool_pw = []
     while event_list:
         register_time = event_list[0].register_start_date.replace(tzinfo=timezone.utc).astimezone(
             tz=pytz.timezone('Europe/Zurich'))
@@ -31,18 +26,11 @@ def check_time():
         if time_delta < 5 * 60:
             event = event_list[0]
             event_list = event_list[1:]
-
-            ASVZ_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
-            with open(os.path.join(ASVZ_DIR, 'key.lock'), 'r') as key_file:
-                key = bytes(key_file.read(), 'utf-8')
-            f = Fernet(key)
-            password = f.decrypt(bytes(event.user.first_name, 'utf-8')).decode('utf-8')
             pool_event.append(event)
-            pool_pw.append(password)
         else:
             break
 
     if pool_event:
         pool = ProcessPool(nodes=10)
-        pool.map(event_subscriber, pool_event, pool_pw)
+        pool.map(subscribe_to_event, pool_event)
     return
