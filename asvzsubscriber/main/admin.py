bcdcf52d3ff4993444ef408f8f1ad55e34dad64f
Frankie <knobelf@student.ethz.ch>
2020-10-23 14:34:26 +0200

    save bearertoken with timer on it

--- asvzsubscriber/main/admin.py
+++ asvzsubscriber/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ASVZEvent
+from .models import ASVZEvent, BearerToken
 
 # Register your models here.
 admin.site.register(ASVZEvent)
+admin.site.register(BearerToken)
