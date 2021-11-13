5af37abe5b1fc115184c0c7d025fd42dc25e08cc
Franz Knobel <knobelf@student.ethz.ch>
2021-11-13 15:48:55 +0100

    rewriting usermodel

--- asvzsubscriber/main/forms.py
+++ asvzsubscriber/main/forms.py
@@ -1,6 +1,7 @@
 from random import random
-
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class EventForm(forms.Form):
@@ -13,3 +14,19 @@ class EventForm(forms.Form):
 
     def set_choices(self, events):
         self.fields['Events'].choices = events
+
+
+class NewUserForm(UserCreationForm):
+    school = forms.ChoiceField()
+
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
