e5ba58b30e517295633866085613ae71b3585da7
franz <29305369+Frankie8472@users.noreply.github.com>
2024-03-06 00:30:22 +0100

    Removing and redefine print statements

--- asvzsubscriber/main/management/commands/timecheck.py
+++ asvzsubscriber/main/management/commands/timecheck.py
@@ -2,7 +2,7 @@
 import time
 import pytz
 from datetime import datetime, timezone
-from pathos.multiprocessing import ProcessPool, freeze_support
+from pathos.multiprocessing import ProcessPool
 from django.core.management import BaseCommand
 from main.asvz_crawler import ASVZCrawler
 from main.models import ASVZEvent
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 
 
 def check_time():
-    print(f"========= Chron Job =========", flush=True)
+    print(f"========= Chron Job - Registration =========", flush=True)
     current_time = datetime.now(tz=pytz.timezone('Europe/Zurich'))
     event_list = ASVZEvent.objects.order_by('register_start_date')
 
@@ -25,9 +25,7 @@ def check_time():
         register_time = event_list[0].register_start_date.replace(tzinfo=timezone.utc).astimezone(
             tz=pytz.timezone('Europe/Zurich'))
         time_delta = (register_time - current_time).total_seconds()
-        print(f"current event: {event_list[0].url[-6:]}")
         if time_delta < 5 * 60:
-            print(f"appending event: {event_list[0].url[-6:]}")
             event = event_list[0]
             event_list = event_list[1:]
             pool_event.append(event)
@@ -40,5 +38,5 @@ def check_time():
         while not res.ready():
             time.sleep(5)
 
-    print(f"========= Finished  =========", flush=True)
+    print(f"========= Finished - Registration  =========", flush=True)
     return
