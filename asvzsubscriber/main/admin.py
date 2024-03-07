fe31b12aacec55ddb3827497e8628ec8595f7c78
franz <29305369+Frankie8472@users.noreply.github.com>
2024-03-07 17:29:33 +0100

    updating admin view with asvztoken

--- asvzsubscriber/main/admin.py
+++ asvzsubscriber/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ASVZEvent, ASVZUser
+from .models import ASVZEvent, ASVZUser, ASVZToken
 
 # Register your models here.
 admin.site.register(ASVZUser)
+admin.site.register(ASVZToken)
 admin.site.register(ASVZEvent)
 
